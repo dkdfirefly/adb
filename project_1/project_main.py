9d912b3af0a2babe3c8f716b8f5e76403d5f422b
sarahpanda <sp3206@columbia.edu>
2014-02-11 20:01:04 -0500

    Basic Rocchio
    An inefficient version of Rocchio.. Just builds the dictionary and returns the topmost (querylength+2 words for the new query.

--- project_1/project_main.py
+++ project_1/project_main.py
@@ -1,6 +1,10 @@
 import urllib2
 import json
 import base64
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk import stem
 
 def main():
   bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?$format=json&Query=%27gates%27&$top=10'
@@ -16,6 +20,15 @@ def main():
   #content contains the xml/json response from Bing.
   positives = []
   negatives = []
+  Query = 'Bill Gates'
+  vocab = dict()
+  q=10
+  a=5
+  b=1
+  for word in preProcess(Query):
+      vocab[word]=q
+          
+          
   #Take input from user
   for result in data['d']['results']:
       print '\nTitle:\n' + result['Title']
@@ -25,14 +38,39 @@ def main():
           var = raw_input("Relevant or irrelevant(y/n)? :")
           if str(var)=='y':
               positives.append(result['Description'])
+              for word in preProcess(result['Description']):
+                  if word not in vocab.keys():
+                      vocab[word]=a
+                  else:
+                      vocab[word]+=a
               flag = 1
           elif str(var)=='n':
               negatives.append(result['Description'])
+              for word in preProcess(result['Description']):
+                  if word not in vocab.keys():
+                      vocab[word]=-b
+                  else:
+                      vocab[word]-=b
               flag = 1
           else:
               print 'Wrong Input. Enter again:'
   print 'precision = ', str(len(positives)*1.0/10)
+  size_new = len(Query.split())+2
+  print 'New Query :\n', sorted(vocab.keys(), key=vocab.get)[-size_new:]
   #print content
 
+def preProcess(text):
+    text=text.lower()
+    stemmer=stem.PorterStemmer()
+    for w in string.punctuation:
+        text=text.replace(w,' ')
+    words = text.split()
+    words = [w for w in words if not w in stopwords.words('english')]
+    #removed stemming for now
+##    stemmed =[]
+##    for w in words:
+##      stemmed.append(stemmer.stem(w))  
+    return words
+    
 if __name__ == '__main__':
   main()
