5b4b98492f57ae898f8a66fd6b5f097e0c84503c
dkdfirefly <dkdfirefly@gmail.com>
2014-03-29 21:13:57 -0400

    added fields

--- project_2/fieldvalues.py
+++ project_2/fieldvalues.py
@@ -62,7 +62,11 @@ sportTeamProp = {"Name": "/type/object/name",
 	   "Locations": "/sports/sports_team/location",
 	   "PlayersRoster": "/sports/sports_team/roster", #compound
 	}
-tvactorProp = {}
+tvactorProp = {
+	}
+	
+professionalSportsTeam = {
+	}
 
 ################ COMPOUND PROPERTIES ###################
 
