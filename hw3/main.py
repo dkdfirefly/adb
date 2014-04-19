77bb212ddf4a64769f83796853553ba983873b39
dkdfirefly <dkdfirefly@gmail.com>
2014-04-19 19:41:35 -0400

    added consistent varNames, comments - basic structure working

--- hw3/main.py
+++ hw3/main.py
@@ -1,22 +1,36 @@
 import itertools
+
+# Hardcoded samples
 sample = [['pen', 'ink', 'diary', 'soap'], ['pen','ink', 'diary'],['pen','diary'], ['pen','ink','soap']]
 
-L = []
-k=0
+# Input params
 min_support = 0.7
 min_conf = 0.8
+
+# Terminologies
+#   candGroup : list of list(candK) of list(cand) => The entire candidate structure
+#   candK : list of list(cand) => candidate set of length K
+#   cand : list => The actual candidate
+
+# Initialisations
+k=0
+candGroup = []
+candK = []
 
 support = dict();
 l0=[]
 for rows in sample:
     for item in rows:
+        itemTuple = (item,)
         try:
-            support[item]+=1
+            support[itemTuple]+=1
         except KeyError:
-            support[item]=1
+            support[itemTuple]=1
 print support
-l0 = [[i] for i in support.keys() if (support[i]*1.0)/len(support.keys()) >= min_support];
-L.append(l0)
+candK = [list(i) for i in support.keys() if (support[i]*1.0)/len(support.keys()) >= min_support];
+candGroup.append(candK)
+print 'group'
+print candGroup
 k+=1
 
 ##### Candidate set #####
@@ -31,27 +45,44 @@ k+=1
 ##L.append(l1)
 ##print L
 
-lnew=[]
+candK = []
 
 #join
-for i in range(len(L[k-1])):
-    for j in range(i+1,len(L[k-1])):
-        if L[k-1][i][:-1]==L[k-1][j][:-1]:
+for candInd1 in range(len(candGroup[k-1])):
+    for candInd2 in range(candInd1+1, len(candGroup[k-1])):
+        # check if all terms till the last one in cand match
+        if candGroup[k-1][candInd1][:-1] == candGroup[k-1][candInd2][:-1]:
            temp = []
-           temp.extend(L[k-1][i])
-           temp.append(L[k-1][j][-1])
-           lnew.append(temp)
-L.append(lnew)
+           # copy over the first cand from (k-1)
+           temp.extend(candGroup[k-1][candInd1])
+           # append the last term which is different
+           temp.append(candGroup[k-1][candInd2][-1])
+           candK.append(temp)
+candGroup.append(candK)
+print 'group'
+print candGroup
 
 #prune
-currentL = L[-1]
-for candidate in currentL:
-     for comb in list(itertools.combinations(candidate,1)):
-         print list(set(comb))
-##         if set(comb) not in L:
-##             currentL.remove(candidate)
-L = L[-1].extend(currentL)
-print L
-  
-            
+candK = candGroup[-1]
+candK_1 = candGroup[-2]
+print 'candK'
+print candK
+print 'candK_1'
+print candK_1
+for cand in candK:
+     # get all the permutations of cand
+     for comb in list(itertools.combinations(cand,k)):
+         print 'comb'
+         print list(comb)
+         # check if each permutation is present in the (k-1)
+         if list(comb) not in candK_1:
+             print 'in'
+             if cand in candK:
+                 candK.remove(cand)
+                 print 'in2'
 
+candGroup.pop()
+candGroup.append(candK)
+
+print 'group'
+print candGroup
